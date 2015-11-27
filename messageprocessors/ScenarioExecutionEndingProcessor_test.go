f4f64134b12692d93fd2dde57ca7a0461f2542cf
Manu Viswam <evmanu@gmail.com>
2015-11-27 20:20:52 +0530

    Unit test for ScenarioExecutionStartingProcessor

---
+++ messageprocessors/ScenarioExecutionEndingProcessor_test.go
@@ -0,0 +1,25 @@
+package messageprocessors
+
+import (
+	m "github.com/manuviswam/gauge-go/gauge_messages"
+	t "github.com/manuviswam/gauge-go/testsuit"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestShouldReturnExecutionStatusResponseWithSameIdForScenarioExecutionEnding(tst *testing.T) {
+	msgId := int64(12345)
+	steps := make([]t.Step, 0)
+
+	msg := &m.Message{
+		MessageType: m.Message_ScenarioExecutionEnding.Enum(),
+		MessageId:   &msgId,
+	}
+
+	p := ScenarioExecutionEndingProcessor{}
+
+	result := p.Process(msg, steps)
+
+	assert.Equal(tst, result.MessageType, m.Message_ExecutionStatusResponse.Enum())
+	assert.Equal(tst, *result.MessageId, msgId)
+}
