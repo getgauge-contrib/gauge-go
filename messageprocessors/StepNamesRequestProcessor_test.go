1dfd52bf173edca4912aa5913de2800d192b6283
Manu Viswam <evmanu@gmail.com>
2015-11-27 20:37:27 +0530

    Unit test for StepNamesRequestProcessor

---
+++ messageprocessors/StepNamesRequestProcessor_test.go
@@ -0,0 +1,47 @@
+package messageprocessors
+
+import (
+	m "github.com/manuviswam/gauge-go/gauge_messages"
+	t "github.com/manuviswam/gauge-go/testsuit"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestShouldReturnStepNamesResponseWithSameIdForStepnamesRequest(tst *testing.T) {
+	msgId := int64(12345)
+	steps := make([]t.Step, 0)
+
+	msg := &m.Message{
+		MessageType: m.Message_StepNamesRequest.Enum(),
+		MessageId:   &msgId,
+	}
+
+	p := StepNamesRequestProcessor{}
+
+	result := p.Process(msg, steps)
+
+	assert.Equal(tst, result.MessageType, m.Message_StepNamesResponse.Enum())
+	assert.Equal(tst, *result.MessageId, msgId)
+}
+
+func TestShouldReturnAllStepNames(tst *testing.T) {
+	stepText := "Step description"
+	msgId := int64(12345)
+	step := t.Step{
+		Description: stepText,
+		Impl:        func() {},
+	}
+	steps := make([]t.Step, 0)
+	steps = append(steps, step)
+
+	msg := &m.Message{
+		MessageType: m.Message_StepNamesRequest.Enum(),
+		MessageId:   &msgId,
+	}
+
+	p := StepNamesRequestProcessor{}
+
+	result := p.Process(msg, steps)
+
+	assert.Contains(tst, result.StepNamesResponse.Steps, stepText)
+}
