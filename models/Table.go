fd21e5fd211fb4430f06fc575d344fe13c0bbd4f
Manu Viswam <evmanu@gmail.com>
2015-12-12 22:49:28 +0530

    New table model

---
+++ models/Table.go
@@ -0,0 +1,44 @@
+package models
+import (
+	m "github.com/getgauge/gauge/gauge_messages"
+)
+
+type Table struct {
+	// / Contains the Headers for the table
+	Headers *TableRow
+	// / Contains the Rows for the table
+	Rows             []*TableRow
+}
+
+type TableRow struct {
+	// / Represents the cells of a given table
+	Cells            []string
+}
+
+func (t *Table) ConvertToProtoTable() *m.ProtoTable {
+	//TODO handle error scenarios
+	p := &m.ProtoTable{
+		Headers: &m.ProtoTableRow{
+			Cells: t.Headers.Cells,
+		},
+	}
+
+	for _, row := range t.Rows {
+		p.Rows = append(p.Rows, &m.ProtoTableRow{ Cells: row.Cells})
+	}
+	return p
+}
+
+func CreateFromProtoTable(p *m.ProtoTable) *Table {
+	//TODO handle error scenarios
+	t := &Table{
+		Headers: &TableRow{
+			Cells: p.Headers.Cells,
+		},
+	}
+
+	for _, row := range p.Rows {
+		t.Rows = append(t.Rows, &TableRow{ Cells: row.Cells})
+	}
+	return t
+}
---
+++ models/Table_test.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	m "github.com/getgauge/gauge/gauge_messages"
+
+)
+
+func TestConvertToProtoTable(t *testing.T) {
+	headers := []string{"Header 1", "Header 2"}
+
+	columns := []string{"Column 1", "Column 2"}
+
+	row := &TableRow{Cells: columns}
+	rows := []*TableRow{row}
+
+	tbl := &Table{
+		Headers: &TableRow{
+			Cells: headers,
+		},
+		Rows: rows,
+	}
+
+	p := tbl.ConvertToProtoTable()
+
+	assert.Contains(t, p.Headers.Cells, "Header 1")
+	assert.Contains(t, p.Headers.Cells, "Header 2")
+	assert.Contains(t, p.Rows[0].Cells, "Column 1")
+	assert.Contains(t, p.Rows[0].Cells, "Column 2")
+}
+func TestCreateFromProtoTable(t *testing.T) {
+	headers := []string{"Header 1", "Header 2"}
+
+	columns := []string{"Column 1", "Column 2"}
+
+	row := &m.ProtoTableRow{Cells: columns}
+	rows := []*m.ProtoTableRow{row}
+
+	p := &m.ProtoTable{
+		Headers: &m.ProtoTableRow{
+			Cells: headers,
+		},
+		Rows: rows,
+	}
+
+	tbl := CreateFromProtoTable(p)
+
+	assert.Contains(t, tbl.Headers.Cells, "Header 1")
+	assert.Contains(t, tbl.Headers.Cells, "Header 2")
+	assert.Contains(t, tbl.Rows[0].Cells, "Column 1")
+	assert.Contains(t, tbl.Rows[0].Cells, "Column 2")
+}
