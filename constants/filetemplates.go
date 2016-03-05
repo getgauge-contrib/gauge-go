5e8bb7effbe264d988ff30ee672dcd5566e89aff
Manu Viswam <evmanu@gmail.com>
2016-03-05 12:00:17 +0530

    Update sample program to use different method signatures

--- constants/filetemplates.go
+++ constants/filetemplates.go
@@ -10,12 +10,11 @@ import (
 	. "github.com/manuviswam/gauge-go/runner"
 )
 
-var _ = Describe("Say <greeting> to <product name>", func(args ...interface{}) {
-	fmt.Println(args[0].(string) + ", " + args[1].(string))
+var _ = Describe("Say <greeting> to <product name>", func(greeting, name string) {
+	fmt.Println(greeting + ", " + name)
 })
 
-var _ = Describe("Step that takes a table <table>", func(args ...interface{}) {
-	tbl := args[0].(*m.Table)
+var _ = Describe("Step that takes a table <table>", func(tbl *m.Table) {
 	for _, columnName := range tbl.Headers.Cells {
 		fmt.Printf("%s,\t", columnName)
 	}
@@ -29,10 +28,9 @@ var _ = Describe("Step that takes a table <table>", func(args ...interface{}) {
 	}
 })
 
-var _ = Describe("A context step which gets executed before every scenario", func(args ...interface{}) {
+var _ = Describe("A context step which gets executed before every scenario", func() {
 	fmt.Println("Context step executed")
 })
-
 `
 
 const InitTestTemplate = `
