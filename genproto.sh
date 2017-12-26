2485ad4d2f779527bf62b5c3e9e1d8cf83ead2c8
BugDiver <vinaysh@thoughtworks.com>
2017-12-26 16:56:01 +0530

    Updating proto messages.

--- genproto.sh
+++ genproto.sh
@@ -2,4 +2,4 @@
 
 #Using protoc version 3.0.0
 cd gauge-proto
-protoc --go_out=../gauge_messages/ api.proto api_v2.proto messages.proto spec.proto
+protoc --go_out=../gauge_messages/ api.proto messages.proto spec.proto
